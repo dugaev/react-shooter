d14c530a24781a3c160a1d4401ad54f7ba737c92
Oleg Dugaev <dugaevoleg9@gmail.com>
2024-01-19 19:28:49 +0200

    comm

--- src/Cube.jsx
+++ src/Cube.jsx
@@ -2,13 +2,14 @@ import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 import cubes from "@/cubes.json";
 import { RigidBody } from "@react-three/rapier";
+import Kym from "@/assets/images/kym.jpg";
 
 export const Cubes = () => {
   return cubes.map((coords, index) => <Cube key={index} position={coords} />);
 };
 
 const Cube = (props) => {
-  const texture = useLoader(TextureLoader, "../src/assets/images/kym.jpg");
+  const texture = useLoader(TextureLoader, Kym);
 
   return (
     <RigidBody {...props}>
@@ -16,7 +17,6 @@ const Cube = (props) => {
         <meshBasicMaterial attach="material" map={texture} />
         <boxGeometry />
       </mesh>
-      {/*  */}
     </RigidBody>
   );
 };
