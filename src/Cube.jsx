5ee52ea590bd37bd5a9ea9e137a4fc762c4fa8c7
Oleg Dugaev <dugaevoleg9@gmail.com>
2024-01-19 19:32:58 +0200

    changed cubes pos

--- src/Cube.jsx
+++ src/Cube.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three";
 import cubes from "@/cubes.json";
@@ -5,7 +6,25 @@ import { RigidBody } from "@react-three/rapier";
 import Kym from "@/assets/images/kym.jpg";
 
 export const Cubes = () => {
-  return cubes.map((coords, index) => <Cube key={index} position={coords} />);
+  // Знаходимо середні значення координат
+  const averageCoords = cubes.reduce(
+    (acc, coords) => {
+      acc[0] += coords[0];
+      acc[1] += coords[1];
+      acc[2] += coords[2];
+      return acc;
+    },
+    [0, 0, 0]
+  );
+
+  // Розраховуємо середні значення
+  averageCoords[0] /= cubes.length;
+  averageCoords[1] /= cubes.length;
+  averageCoords[2] /= cubes.length;
+
+  return cubes.map((coords, index) => (
+    <Cube key={index} position={subtractCoords(coords, averageCoords)} />
+  ));
 };
 
 const Cube = (props) => {
@@ -20,3 +39,12 @@ const Cube = (props) => {
     </RigidBody>
   );
 };
+
+// Віднімання координат
+function subtractCoords(coords, subtractor) {
+  return [
+    coords[0] - subtractor[0],
+    coords[1] - subtractor[1],
+    coords[2] - subtractor[2],
+  ];
+}
